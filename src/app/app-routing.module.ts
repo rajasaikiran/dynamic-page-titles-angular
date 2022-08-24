4509d45627d2a0dc52528e036b1ad7cc83f5c428
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-08-24 16:49:04 +0530

    title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path :'home' , component : HomeComponent },
+  {  path :'about' , component : AboutUsComponent },
+  {  path :'contact' , component : ContactUsComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css']
+})
+export class ContactUsComponent {
+
+  constructor(private titlepage : Title) { 
+    titlepage.setTitle('Contact-us')
+  }
+
+}
